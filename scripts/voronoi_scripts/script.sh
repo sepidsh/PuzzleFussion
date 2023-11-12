42f7e2c832b47ee5b48eb934693cf6e3f590c32a
Sepid Hosseini <56614286+sepidsh@users.noreply.github.com>
2023-11-11 18:03:56 -0800

    Update script.sh

--- scripts/voronoi_scripts/script.sh
+++ scripts/voronoi_scripts/script.sh
@@ -1,4 +1,4 @@
 MODEL_FLAGS="--dataset voronoi --batch_size 1524 --set_name train"
 TRAIN_FLAGS="--lr 1e-3 --save_interval 5000 --weight_decay 0.05 --log_interval 1000 --use_image_features False"
 #SAMPLE_FLAGS="--batch_size 1024 --num_samples 32 --set_name test" 
-mpiexec -n 2 python image_train.py $MODEL_FLAGS $TRAIN_FLAGS --exp_name voronoi_ckpt_new 
+mpiexec -n 2 python image_train.py $MODEL_FLAGS $TRAIN_FLAGS --exp_name preds
