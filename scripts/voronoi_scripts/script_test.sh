76b7240d89915454e3b97c60780f2d8a1ed8eb37
Sepid Hosseini <56614286+sepidsh@users.noreply.github.com>
2023-11-11 18:03:43 -0800

    Update script_test.sh

--- scripts/voronoi_scripts/script_test.sh
+++ scripts/voronoi_scripts/script_test.sh
@@ -1,2 +1,2 @@
 SAMPLE_FLAGS="--num_samples 2000  --dataset voronoi --batch_size 1024  --set_name test --use_image_features False"
-CUDA_VISIBLE_DEVICES='0' python image_sample.py  --set_name test --model_path ckpts/voronoi_ckpt_new/model300000.pt  $SAMPLE_FLAGS
+CUDA_VISIBLE_DEVICES='0' python image_sample.py  --set_name test --model_path ckpts/preds/model300000.pt  $SAMPLE_FLAGS
